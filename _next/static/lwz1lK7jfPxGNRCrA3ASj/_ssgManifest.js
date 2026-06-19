1dbbeb5de6caf2a74babdc61c5b13c1a48f5a4de
KbalaRecoxx <k.balakayan@recoxx.com>
2026-06-19 21:48:31 +0800

    init

---
+++ _next/static/lwz1lK7jfPxGNRCrA3ASj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Fgive","\u002F[locale]\u002Fministries","\u002F[locale]\u002Fsermons","\u002F[locale]\u002Ftestimonies","\u002F[locale]\u002Fvisit"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
